e877f56548f8f667e4db06529f06ed918e9b8bfa
Kuldeep Singh <kuldeepsingh@Kuldeeps-MacBook-Air-2.local>
2023-06-20 18:24:05 +0530

    it's all done

---
+++ design-patterns/singleton/use-case/game2.ts
@@ -0,0 +1,14 @@
+// A Game Class that uses the Leaderboard Singleton
+
+import Leaderboard from "./leaderboard";
+import Game from "./igame";
+
+export default class Game2 implements Game {
+    leaderboard: Leaderboard;
+    constructor() {
+        this.leaderboard = new Leaderboard();
+    }
+    addWinner(position: number, name: string): void {
+        this.leaderboard.addWinner(position, name);
+    }
+}
